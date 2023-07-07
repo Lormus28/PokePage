53fc3aaf61d5e92358cce7f07458c2454fccd1de
laura.posadaaz@gmail.com <you@example.com>
2023-07-07 10:17:47 -0500

    pokemon. js functional

--- pokemon.js
+++ pokemon.js
@@ -2,51 +2,48 @@ const $containerPokemons = document.querySelector(".container-pokemons");
 
 const $linksPrevNext = document.querySelector(".links-prev-next");
 
-
 let APIpokemon = "https://pokeapi.co/api/v2/pokemon/";
 
-
 async function pokemonData(url) {
-  let APITypes ="https://pokeapi.co/api/v2/type";
+  let APITypes = "https://pokeapi.co/api/v2/type";
 
-  if (localStorage.getItem("selectType")===null){
-    localStorage.setItem("selectType","all");
+  if (localStorage.getItem("selectType") === null) {
+    localStorage.setItem("selectType", "all");
   }
 
   try {
-
     $containerPokemons.innerHTML = `<img class="loader" src="./assets/loader.svg" alt="Loading...">`;
 
-    let typerute = localStorage.getItem("selectType") === "all"
-    ? url 
-    : `${APITypes}/${localStorage.getItem("selectType")}`;
+    let typerute =
+      localStorage.getItem("selectType") === "all"
+        ? url
+        : `${APITypes}/${localStorage.getItem("selectType")}`;
 
     let res = await fetch(typerute),
-    json = await res.json(),
-    $template = "",
-    $prevLink,
-    $nextLink;
-    
+      json = await res.json(),
+      $template = "",
+      $prevLink,
+      $nextLink;
+
     //console.log(json);
 
-    let pokemonjson = localStorage.getItem("selectType") === "all" 
-    ? json.results
-    : json.pokemon;
+    let pokemonjson =
+      localStorage.getItem("selectType") === "all"
+        ? json.results
+        : json.pokemon;
 
-    for (let i = 0; i < pokemonjson.length; i++){
-      //console.log(json.results[i]);
+    for (let i = 0; i < pokemonjson.length; i++) {
       try {
         let res = await fetch(
-          localStorage.getItem("selectType") === "all" 
-          ? pokemonjson[i].url
-          : pokemonjson[i].pokemon.url
-        ),
-
-        pokemon = await res.json();
+            localStorage.getItem("selectType") === "all"
+              ? pokemonjson[i].url
+              : pokemonjson[i].pokemon.url
+          ),
+          pokemon = await res.json();
         // console.log (res, pokemon);
-  
+
         if (!res.ok) throw { status: res.status, statusText: res.statusText };
-  
+
         $template += `
         <div class="pokeCard">
             <div class="name">
@@ -66,45 +63,49 @@ async function pokemonData(url) {
         // console.log(err);
         let message = err.statusText || "Unexpected error";
         $template.innerHTML = `<div class="pokeCard">
-            <div class="name">
-              <p><b>Name</b></p>
-              <i class="fa-regular fa-heart"></i>
-            </div>
-            <div class="backCard">
-              <p>Error ${err.status}: ${message}</p>
-            </div>
-            <div class="powerLevel">
-             <p><b>Power Level</b></p>
-             <button>Buy</button>
-            </div>
-          </div>`;
+                    <div class="name">
+                      <p><b>Name</b></p>
+                      <i class="fa-regular fa-heart"></i>
+                    </div>
+                    <div class="backCard">
+                      <p>Error ${err.status}: ${message}</p>
+                    </div>
+                    <div class="powerLevel">
+                     <p><b>Power Level</b></p>
+                     <button>Buy</button>
+                    </div>
+                  </div>`;
       }
     }
+
     $containerPokemons.innerHTML = $template;
 
-    $nextLink = json.next ? `<a class="nextPrevButton" href="${json.next}">➡️</a>` : "";
-    $prevLink = json.previous ? `<a class= "nextPrevButton" href="${json.previous}">⬅️</a>` : "";
+    $nextLink = json.next
+      ? `<a class="nextPrevButton" href="${json.next}">➡️</a>`
+      : "";
+    $prevLink = json.previous
+      ? `<a class= "nextPrevButton" href="${json.previous}">⬅️</a>`
+      : "";
     $linksPrevNext.innerHTML = $prevLink + " " + $nextLink;
+  } catch (err) {
+    let message = err.statusText || "Unexpected error";
+    $containerPokemons.innerHTML = `<p> Error ${err.status}:${message}</p>`;
+  }
+}
 
-     //if (!res.ok) throw { status: res.status, statusText: res.statusText };
-    }catch (err) {
-      let message = err.statusText || "Unexpected error";
-      $containerPokemons.innerHTML = `<p> Error ${err.status}:${message}</p>`;
-  } 
-
-  document.addEventListener("click", (e) => {
+document.addEventListener("click", (e) => {
     if (e.target.matches(".links-prev-next a")) {
       e.preventDefault();
       pokemonData(e.target.getAttribute("href"));
     }
   });
 
-  document.addEventListener("DOMContentLoaded", (e) => pokemonData(APIpokemon));
+document.addEventListener("DOMContentLoaded", (e) => pokemonData(APIpokemon));
 
-  document.addEventListener("click", (e) => {
-    if (e.target.matches(".pokemonTypes a"))
-    {localStorage.setItem("selectType", e.target.id)}
+document.addEventListener("click", (e) => {
+    if (e.target.matches(".pokemonTypes a")) {
+      localStorage.setItem("selectType", e.target.id);
+    }
   });
 
 
-};
